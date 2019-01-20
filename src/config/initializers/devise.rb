05a4d6b9d5d29484dbcaaa3a1f3815fe6f4f6e90
Nguyen Nguyen <thaonguyen.nguyenviet@gmail.com>
2019-01-20 10:12:38 +0700

    add view devise not work

--- src/config/initializers/devise.rb
+++ src/config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'bca0148bda3c184f360f2447bd096221fb55e058b4420ca6f13c076867b87105f4dca03826aff31f462d685135a7721e9f529dda79a3fa253c2a1fd47f2d18ed'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -229,7 +229,7 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
