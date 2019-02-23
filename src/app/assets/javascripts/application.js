4be4424ebe276823e49e8c349843c6fe2d163c80
tamlh <tamlh@atmarkcafe.org>
2019-02-23 16:09:14 +0700

    Add layout admin

--- src/app/assets/javascripts/application.js
+++ src/app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require admin/global
 //= require_tree .
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  resources :members
-  resources :users
-  resources :price_lists
-  resources :passengers
-  resources :customers
-  resources :car_rental_contracts
-  resources :investment_contracts
-  resources :drivers
-  resources :user_types
-  resources :cars
-  resources :car_models
-  resources :car_brands
+  scope :admin_site do
+    resources :members
+    resources :users
+    resources :price_lists
+    resources :passengers
+    resources :customers
+    resources :car_rental_contracts
+    resources :investment_contracts
+    resources :drivers
+    resources :user_types
+    resources :cars
+    resources :car_models
+    resources :car_brands
+  end
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
