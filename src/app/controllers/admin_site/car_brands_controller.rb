39894694042b4ef0be829ace0af97ce08b0e79e8
tamlh <tamlh@atmarkcafe.org>
2019-02-23 12:16:37 +0700

    not use

---
+++ src/app/controllers/admin_site/car_brands_controller.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+class AdminSite::CarBrandsController < AdminSite::BaseController
+  before_action :set_car_brand, only: %i[show edit update destroy]
+
+  # GET /car_brands
+  # GET /car_brands.json
+  def index
+    @car_brands = CarBrand.all
+  end
+
+  # GET /car_brands/1
+  # GET /car_brands/1.json
+  def show; end
+
+  # GET /car_brands/new
+  def new
+    @car_brand = CarBrand.new
+  end
+
+  # GET /car_brands/1/edit
+  def edit; end
+
+  # POST /car_brands
+  # POST /car_brands.json
+  def create
+    @car_brand = CarBrand.new(car_brand_params)
+    respond_to do |format|
+      if @car_brand.save
+        format.html { redirect_to admin_site_car_brand_path(@car_brand), notice: 'Car brand was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /car_brands/1
+  # PATCH/PUT /car_brands/1.json
+  def update
+    respond_to do |format|
+      if @car_brand.update(car_brand_params)
+        format.html { redirect_to redirect_to admin_site_car_brand_path(@car_brand), notice: 'Car brand was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /car_brands/1
+  # DELETE /car_brands/1.json
+  def destroy
+    @car_brand.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_site_car_brands_path, notice: 'Car brand was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_car_brand
+    @car_brand = CarBrand.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def car_brand_params
+    params.require(:car_brand).permit(:code, :name)
+  end
+end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -3,18 +3,20 @@
 Rails.application.routes.draw do
   namespace :admin_site do
     resources :members
-    resources :users
-    resources :price_lists
-    resources :passengers
-    resources :customers
-    resources :car_rental_contracts
-    resources :investment_contracts
-    resources :drivers
-    resources :user_types
-    resources :cars
-    resources :car_models
     resources :car_brands
   end
+
+  resources :users
+  resources :price_lists
+  resources :passengers
+  resources :customers
+  resources :car_rental_contracts
+  resources :investment_contracts
+  resources :drivers
+  resources :user_types
+  resources :cars
+  resources :car_models
+
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
